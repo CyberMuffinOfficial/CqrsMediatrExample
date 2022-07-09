a9d2a268537e9a9d96110fe8d34246c097c7b705
CyberMuffin <cybermuffinofficial@gmail.com>
2022-07-09 14:36:48 +0530

    Added getProductById handler

---
+++ Handlers/GetProductByIdHandler.cs
@@ -0,0 +1,18 @@
+﻿using CqrsMediatrExample.Entities;
+using CqrsMediatrExample.Persistence;
+using CqrsMediatrExample.Queries;
+using MediatR;
+
+namespace CqrsMediatrExample.Handlers;
+
+public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, Product>
+{
+    private readonly FakeDataStore _fakeDataStore;
+
+    public GetProductByIdHandler(FakeDataStore fakeDataStore) => _fakeDataStore = fakeDataStore;
+
+    public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _fakeDataStore.GetProductById(request.Id);
+    }
+}
